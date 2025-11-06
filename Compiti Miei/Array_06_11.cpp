af3ed57581f316779152f567fd2576fd4122837e
macriGiuseppe3F <macri.giuseppe.2023@itisgalileiroma.it>
2025-11-06 09:44:01 +0100

    06/11/2025

---
+++ Compiti Miei/Array_06_11.cpp
@@ -0,0 +1,17 @@
+//Ordinare gli elementi di un array in ordine crescente.
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int array[i];
+    int array[j];
+    for(int i = 0;i<10; i++){
+        for(int j = 0; j<i; j++)
+            if(array[])
+
+          }
+    
+
+    return 0;
+}
