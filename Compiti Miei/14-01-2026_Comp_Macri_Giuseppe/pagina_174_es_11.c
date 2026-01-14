2881d0e57015f858181e215ea7f96551bf8ed05c
Giuseppe <macri.giuseppe.2023@itisaglileiroma.it>
2026-01-14 17:47:53 +0100

    14/01/2026

---
+++ Compiti Miei/14-01-2026_Comp_Macri_Giuseppe/pagina_174_es_11.c
@@ -0,0 +1,10 @@
+/*Scrivi un programma che inserite in input N coppie di numeri stampi
+quante volte i due numeri della coppia sono uguali fra loro*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float N, cont = 0, n1, n2;
+
+printf("Inserisci il primo numero della coppia");
+scanf()
