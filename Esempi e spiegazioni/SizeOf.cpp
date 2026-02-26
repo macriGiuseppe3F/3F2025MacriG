90799d6222db68a97ca1c6d15bfaad927a18e90c
Gabriele <capitani.gabriele.stud@itisgalileiroma.it>
2026-02-26 09:29:50 +0100

    26/02/2026

--- Esempi e spiegazioni/SizeOf.cpp
+++ Esempi e spiegazioni/SizeOf.cpp
@@ -11,7 +11,8 @@ using namespace std;
 int main(){
 
     int vettore[] = {10,20,30,40,50};
-    cout<<"Byte totali:"<<sizeof(vettore);
+
+    cout<<"Byte totali:"          <<sizeof(vettore);
     cout<<"\nByte di un elemento:"<<sizeof(vettore[0]);
-    cout<<"\nElementi totali:"<< sizeof(vettore) / sizeof(vettore[0]);
+    cout<<"\nElementi totali:"    << sizeof(vettore) / sizeof(vettore[0]);
 }
