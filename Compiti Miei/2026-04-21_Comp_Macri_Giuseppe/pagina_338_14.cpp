c4be38fe54a3ddea36b0b3d7741ce315becbeaf5
Giuseppe <macri.giuseppe.2023@itisaglileiroma.it>
2026-04-17 19:11:45 +0200

    17/04/2026

---
+++ Compiti Miei/2026-04-21_Comp_Macri_Giuseppe/pagina_338_14.cpp
@@ -0,0 +1,20 @@
+/*Data una matrice 4×4 di interi, trova la riga o la colonna con somma più alta.*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int matrice[4][4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> matrice[i][j];
+        }
+    }
+
+    // esercizio non capito bene.
+    return 0;
+}
