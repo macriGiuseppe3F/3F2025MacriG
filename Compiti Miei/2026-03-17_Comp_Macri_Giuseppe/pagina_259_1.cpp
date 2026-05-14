88278ce15bdfc8da078bf761277b5c0f99cef73e
Gabriele <capitani.gabriele.stud@itisgalileiroma.it>
2026-05-14 08:14:56 +0200

    14/04/2026

--- Compiti Miei/2026-03-17_Comp_Macri_Giuseppe/pagina_259_1.cpp
+++ Compiti Miei/2026-03-17_Comp_Macri_Giuseppe/pagina_259_1.cpp
@@ -5,27 +5,18 @@
 using namespace std;
 int main()
 {
-
-    int v[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int N = 10;
+    int v = {1,2,3,4,5,6,7,8,9,10};
+    int n= 10;
     int x;
 
-    cout << "Inserisci l'indice: ";
-    cin >> x;
+    cout<<"Inserisci l'indice:";<<endl;
+    cin>>x;
 
-    if (x >= 0 && x < N)
-    {
-        v[x] = v[x] * 2;
-    }
-    else
-    {
-        cout << "Indice non valido" << endl;
-    }
+    for(int i = 0;i<n; i++){
+        v[x] = v[x]*2;
+        v[i]=v[x];
+        cout<<v[i]<<" ";
 
-    for (int i = 0; i < N; i++)
-    {
-        cout << " " << v[i];
-    }
-
+        }
     return 0;
 }
---
+++ prova.html/loginControllerUnsafe.php
@@ -0,0 +1,3 @@
+<?php
+
+    $conn=mysqli_connection("localhost","adolivo","","my_vmartucci";)
